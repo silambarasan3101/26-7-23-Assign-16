ae60df14df0e184a1efc5176de04738bba301640
Silambarasan <msilambarasan98@gmail.com>
2023-07-26 15:42:55 +0530

    first commit

---
+++ BankAccount.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _26_7_23_Assign_16
+{
+    internal class BankAccount
+    {
+        public int accNo { get; }
+        public string name { get; }
+        public double balance { get; set; }
+        public BankAccount(string name)
+        {
+            this.name = name;
+            balance = 0;
+            accNo = GenerateAccNo();
+        }
+        private int GenerateAccNo()
+        {
+            Random rand = new Random();
+            int num = rand.Next(5000);
+            return num;
+        }
+        public bool Deposit(double amount)
+        {
+            if (amount > 0)
+                balance += amount;
+            else
+            {
+                Console.WriteLine("Invalid deposit amount");
+                return false;
+            }
+            return true;
+        }
+        public bool Withdraw(double amount)
+        {
+            if (amount > 0 && amount < balance)
+                balance -= amount;
+            else
+            {
+                Console.WriteLine("Invalid withdrawal amount");
+                return false;
+            }
+            return true;
+        }
+        public void Display()
+        {
+            Console.WriteLine($"Account Number : {accNo}");
+            Console.WriteLine($"Account Holder Name : {name}");
+            Console.WriteLine($"Balance Remaining : {balance}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _26_7_23_Assign_16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                BankAccount bank = new BankAccount("Str");
+                bool flag = true;
+                flag = bank.Deposit(3000);
+                flag = bank.Withdraw(500);
+                if (flag)
+                    bank.Display();             
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
